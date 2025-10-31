2dbc55537d9e4769dfc8942368fcfa35ea267fef
Verbena Macbook Air <vassidiqi@gmail.com>
2025-10-31 12:42:23 +0700

    Update bio to include contacts and CV

--- src/app/features/bio/bio.component.ts
+++ src/app/features/bio/bio.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Definition } from './elements/definition.component';
 import { Loader } from '../../shared/loader/loader.component';
+import { NgOptimizedImage } from '@angular/common';
+import { DeviceService } from '../../core/services/device.service';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { lucideDownload, lucideMail } from '@ng-icons/lucide';
+import { ThemeService } from '../../core/services/theme.service';
 
 @Component({
   selector: 'bio',
   templateUrl: './bio.component.html',
   styleUrl: './bio.component.css',
-  imports: [Definition, Loader],
-  standalone: true
+  imports: [Definition, Loader, NgOptimizedImage, NgIcon],
+  standalone: true,
+  viewProviders: [provideIcons({ lucideMail, lucideDownload })]
 })
-export class Bio {}
+export class Bio {
+  device = inject(DeviceService)
+  theme = inject(ThemeService)
+}
